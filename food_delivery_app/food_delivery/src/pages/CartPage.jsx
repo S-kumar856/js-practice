101a9a7d30b28ed3c3f67dab4898f6f5655c3004
S KUMAR <hk856856@gmail.com>
2025-02-17 22:50:02 +0530

    card

--- food_delivery_app/food_delivery/src/pages/CartPage.jsx
+++ food_delivery_app/food_delivery/src/pages/CartPage.jsx
@@ -4,7 +4,7 @@ import { useAppContext } from '../components/AppContext';
 const CartPage = () => {
     const { cart, removeFromCart } = useAppContext();
     const [quantities, setQuantities] = useState({});
-    const [showCheckout, setShowCheckout] = useState(false)
+    // const [showCheckout, setShowCheckout] = useState(false)
 
     const handleAdd = (id) => {
         setQuantities((prev) => ({
@@ -27,37 +27,32 @@ const CartPage = () => {
         }, 0);
     };
 
-    const check = () =>{
-        setShowCheckout(true)
-    }
+    // const check = () =>{
+    //     setShowCheckout(true)
+    // }
 
     return (
         <>
-        {showCheckout ? (
-            <div className="cart-checkout">
-                checkout
-            </div>
-        ):(
-
-            <div className="p-5">
+            <div className="p-5 w-full flex flex-col items-center justify-center">
+            <div className=' rounded bg-amber-50 border p-8'>
                 <h1 className="text-2xl font-bold mb-4">Your Cart</h1>
                 {cart.length === 0 ? (
                     <p>Your cart is empty.</p>
                 ) : (
                     cart.map((item, index) => (
-                        <div key={index} className="flex items-center justify-between p-3 border-b">
+                        <div key={index} className="flex items-center gap-5 justify-between p-3 border-b">
                             <div>
-                                <h2 className="text-lg font-semibold">{item.biryani_name}</h2>
+                                <h2 className="text-[1rem] font-semibold">{item.biryani_name}</h2>
                                 <p>Price: ₹{item.price.single}</p>
                             </div>
-                            <div className='border px-3 py-1 flex gap-3.5 items-center outline-0 rounded bg-green-100'>
-                                <button className='md:text-3xl cursor-pointer' onClick={() => handleRemove(item.id)}>-</button>
-                                <span>{quantities[item.id] || 1}</span>
-                                <button className='text-2xl cursor-pointer' onClick={() => handleAdd(item.id)}>+</button>
+                            <div className='border px-2 py-1  flex gap-3.5 items-center outline-0 rounded bg-green-100'>
+                                <button className='text-[0.9rem] cursor-pointer' onClick={() => handleRemove(item.id)}>-</button>
+                                <span className='text-[0.9rem]'>{quantities[item.id] || 1}</span>
+                                <button className=' cursor-pointer' onClick={() => handleAdd(item.id)}>+</button>
                             </div>
                             <button
                                 onClick={() => removeFromCart(item.id)}
-                                className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-400"
+                                className="px-3 py-1 bg-red-500 text-white rounded cursor-pointer hover:bg-red-400"
                             >
                                 Remove
                             </button>
@@ -65,16 +60,17 @@ const CartPage = () => {
 
                     ))
                 )}
-                <div className='mt-5 flex justify-evenly'>
-                <div className=" text-xl font-bold">
-                    Total Price: ₹{getTotalPrice()}
-                </div>
-                <div onClick={check} className='text-xl  border px-3 py-2 cursor-pointer hover:bg-black hover:text-white rounded'>
-                    Check Out
+                <div className='mt-5 flex justify-between'>
+                    <div className=" text-xl font-bold">
+                        Total Price: ₹{getTotalPrice()}
+                    </div>
+                    <div className='text-lg  border px-3 py-2 cursor-pointer hover:bg-black hover:text-white rounded'>
+                        Check Out
+                    </div>
                 </div>
                 </div>
             </div>
-        )}
+
         </>
     );
 };
